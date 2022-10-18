cc7951aa12d90571d82678d4940944b946e6cec1
Alison Venancio <alison.venancio@gmail.com>
2022-10-18 15:30:36 -0400

    adding blink app

--- blink_app/app/src/main/java/com/blink/blinkapp/MainActivity.kt
+++ blink_app/app/src/main/java/com/blink/blinkapp/MainActivity.kt
@@ -2,23 +2,43 @@ package com.blink.blinkapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.ToggleButton
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import vendor.blink.ledmanager.LedManager
 
 class MainActivity : AppCompatActivity() {
 
-    val ledManager: LedManager = LedManager.getInstance()
+    private lateinit var ledManager: LedManager
+    private var buttonIsChecked : Boolean = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toggle: ToggleButton = findViewById(R.id.toggleButton)
+        ledManager = LedManager.getInstance();
+        val toggle: ToggleButton = findViewById(R.id.toggleButton);
+
         toggle.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked)
-                    ledManager.ledON()
-                else
-                    ledManager.ledOFF()
+            buttonIsChecked = isChecked
+        }
+
+        GlobalScope.launch {
+            blink()
+        }
+    }
+
+    suspend fun blink() = GlobalScope.async {
+        while (true) {
+            while (buttonIsChecked) {
+                ledManager.ledON()
+                Thread.sleep(1000)
+                ledManager.ledOFF()
+                Thread.sleep(1000)
+            }
         }
     }
 }
